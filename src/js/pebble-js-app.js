bb3426807b08ac7fc33be568fe441dd8fd515ead
Natasha Kerensikova <natacha@instinctive.eu>
2015-12-07 18:11:23 +0000

    Use localStorage and parameter parsing to keep UI settings
    Note that localStorage is used in `pebble-js-app.js` rather than directly
    in `config.html` because the latter has a localStorage shared with all
    pages from `cdn.rawgit.com`, including other apps.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,3 +1,30 @@
+const settings = {  /* "name in local storage": "form input parameter" */
+  "backgroundColor":     "background",
+  "handColor":           "hands",
+  "hourColor":           "hours",
+  "innerColor":          "inner",
+  "minuteColor":         "minutes",
+  "lowBatteryLevel":     "battlvl",
+  "batteryColor":        "battcol",
+  "bluetoothColor":      "bluetooth",
+  "bluetoothVibration":  "vibrate",
+  "textColor":           "textcol",
+  "textFormat":          "textfmt",
+};
+
+function encodeStored(names) {
+  var first = true;
+  var result = "";
+  for (var key in names) {
+    var value = localStorage.getItem(key);
+    if (value != null) {
+      result = result + (first ? "?" : "&") + names[key] + "=" + encodeURIComponent(value);
+      first = false;
+    }
+  }
+  return result;
+}
+
 Pebble.addEventListener("ready", function() {
   console.log("Classic-Lite PebbleKit JS ready!");
 });
@@ -6,11 +33,15 @@ Pebble.addEventListener("showConfiguration", function() {
 /* TODO: switch to release version:
   Pebble.openURL("https://cdn.rawgit.com/faelys/classic-lite/v1.0/config.html");
 */
-  Pebble.openURL("https://rawgit.com/faelys/classic-lite/master/config.html");
+  Pebble.openURL("https://rawgit.com/faelys/classic-lite/trunk/config.html" + encodeStored(settings));
 });
 
 Pebble.addEventListener("webviewclosed", function(e) {
   var configData = JSON.parse(decodeURIComponent(e.response));
+
+  for (var key in settings) {
+    localStorage.setItem(key, configData[key]);
+  }
 
   var dict = {
     1: parseInt(configData["backgroundColor"]),
